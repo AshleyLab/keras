03b70fb860166fb9de371773c4a154a89c84bc2c
Chuan-Sheng Foo <csfoo@cs.stanford.edu>
2015-12-20 03:33:43 -0800

    Add PWM constraint for conv filters
    * Implements projection onto a (probability) simplex to enable the
      constraint to be enforced

--- keras/constraints.py
+++ keras/constraints.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 from . import backend as K
 
+import numpy as np
 
 class Constraint(object):
     def __call__(self, p):
@@ -35,10 +36,61 @@ class UnitNorm(Constraint):
     def __call__(self, p):
         return p / K.sqrt(K.sum(K.square(p), axis=-1, keepdims=True))
 
+
+def _simplex_projection(v, s=1):
+    assert s > 0
+    # check that v is a vector (theano specific code)
+    assert K.ndim(v) == 1
+
+    # get the array of cumulative sums of a sorted (decreasing) copy of v
+    u = K.sort(v)[::-1]
+    cssv = K.cumsum(u)
+    rho = K.nonzero(K.ge(u * K.arange(1, K.shape(v)[0]+1), cssv - s))[0][-1]
+    # compute the Lagrange multiplier associated to the simplex constraint
+    theta = (cssv[rho] - s) / (rho + 1.0)
+    # compute the projection by thresholding v using theta
+    p = K.maximum(v - theta, 0)
+
+    #if v.sum() == s and np.alltrue(v >= 0):
+    #    # best projection: itself!
+    #    return v
+    # else we will return the projection p
+    return K.ifelse(K.and_(K.equal(v.sum(), s), K.all(K.ge(v, 0))),
+                    v, p)
+
+
+def _filter_simplex_projection(W, s=1):
+    assert s > 0
+
+    n, c, h, w = W.shape
+    # check that c == 1
+
+    u = K.sort(W, axis=2)[:, :, ::-1, :]
+    cssv = K.cumsum(u, axis=2)
+
+    cond = (u * K.arange(1, h+1)[:, np.newaxis]) > (cssv - s)
+    # This finds the largest index that is still True by doing an argmax on the
+    # flipped array, as argmax returns the earliest index that is the max.
+    rho = (h - K.argmax(cond[:, :, ::-1, :], axis=2) - 1).flatten()
+    indices = (K.repeat_elements(K.arange(n), w, axis=0),
+               0,
+               rho,
+               K.tile(K.arange(w), n))
+    theta = (((cssv[indices] - s) / (rho + 1.0))
+                .reshape((n, 1, 1, w)))
+    p = K.maximum(W - theta, 0.0)
+    return K.cast(p, 'float32')
+
+
+class PWMSimplex(Constraint):
+    def __call__(self, p):
+        return _filter_simplex_projection(p)
+
 identity = Constraint
 maxnorm = MaxNorm
 nonneg = NonNeg
 unitnorm = UnitNorm
+pwmsimplex = PWMSimplex
 
 from .utils.generic_utils import get_from_module
 def get(identifier, kwargs=None):
