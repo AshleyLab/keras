a6ce786f6f4b90b9c5a100d42c9a2e2f9aa1609a
anna shcherbina <annashcherbina@gmail.com>
2016-03-28 16:57:07 -0700

    updated for consistency with current bleeding edge keras

--- keras/backend/theano_backend.py
+++ keras/backend/theano_backend.py
@@ -148,10 +148,11 @@ def prod(x, axis=None, keepdims=False):
     '''
     return T.prod(x, axis=axis, keepdims=keepdims)
 
-
 def mean(x, axis=None, keepdims=False):
-    return T.mean(x, axis=axis, keepdims=keepdims)
-#                    ,dtype=x.dtype)
+    dtype = None
+    if 'int' in x.dtype:
+        dtype = _FLOATX
+    return T.mean(x, axis=axis, keepdims=keepdims, dtype=dtype)
 
 
 def std(x, axis=None, keepdims=False):
