eb4b988006796e9758d9329649b0f80eb7b5bdd5
AvantiShri <avanti.shrikumar@gmail.com>
2017-01-13 09:25:55 -0800

    RevCompConv1DBatchNorm initial implementation

--- keras/layers/normalization.py
+++ keras/layers/normalization.py
@@ -156,3 +156,115 @@ class BatchNormalization(Layer):
                   'momentum': self.momentum}
         base_config = super(BatchNormalization, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
+
+
+class RevCompConv1DBatchNorm(Layer):
+    '''Batch norm that shares weights over reverse complement channels
+    '''
+    def __init__(self, epsilon=1e-3, mode=0, axis=-1, momentum=0.99,
+                 weights=None, beta_init='zero', gamma_init='one',
+                 gamma_regularizer=None, beta_regularizer=None, **kwargs):
+        self.supports_masking = True
+        self.beta_init = initializations.get(beta_init)
+        self.gamma_init = initializations.get(gamma_init)
+        self.epsilon = epsilon
+        self.mode = mode
+        assert axis==-1 or axis==2, "Intended for Conv1D"
+        self.axis = axis
+        self.momentum = momentum
+        self.gamma_regularizer = regularizers.get(gamma_regularizer)
+        self.beta_regularizer = regularizers.get(beta_regularizer)
+        self.initial_weights = weights
+        if self.mode == 0:
+            self.uses_learning_phase = True
+        super(BatchNormalization, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.input_spec = [InputSpec(shape=input_shape)]
+        self.num_input_chan = input_shape[self.axis]
+        self.input_len = input_shape[1]
+        assert self.input_len is not None,\
+         "not implemented for undefined input len"
+        assert self.num_input_chan%2 == 0, "should be even for revcomp input"
+        shape = (int(self.num_input_chan/2),)
+
+        self.gamma = self.add_weight(shape,
+                                     initializer=self.gamma_init,
+                                     regularizer=self.gamma_regularizer,
+                                     name='{}_gamma'.format(self.name))
+        self.beta = self.add_weight(shape,
+                                    initializer=self.beta_init,
+                                    regularizer=self.beta_regularizer,
+                                    name='{}_beta'.format(self.name))
+        self.running_mean = self.add_weight(shape, initializer='zero',
+                                            name='{}_running_mean'.format(self.name),
+                                            trainable=False)
+        self.running_std = self.add_weight(shape, initializer='one',
+                                           name='{}_running_std'.format(self.name),
+                                           trainable=False)
+
+        if self.initial_weights is not None:
+            self.set_weights(self.initial_weights)
+            del self.initial_weights
+        self.built = True
+
+    def call(self, x, mask=None):
+        orig_x = x
+        #create a fake x by concatentating reverse-complemented pairs
+        #along the length dimension
+        x = K.concatenate(
+            tensors=[x[:,:,:int(self.num_input_chan/2)],
+                     x[:,:,int(self.num_input_chan/2):][:,:,::-1]],
+            axis=1)
+        if self.mode == 0 or self.mode == 2:
+            assert self.built, 'Layer must be built before being called'
+            input_shape = K.int_shape(x)
+
+            reduction_axes = list(range(len(input_shape)))
+            del reduction_axes[self.axis]
+            broadcast_shape = [1] * len(input_shape)
+            broadcast_shape[self.axis] = input_shape[self.axis]
+
+            x_normed, mean, std = K.normalize_batch_in_training(
+                x, self.gamma, self.beta, reduction_axes,
+                epsilon=self.epsilon)
+
+            if self.mode == 0:
+                self.add_update([K.moving_average_update(self.running_mean, mean, self.momentum),
+                                 K.moving_average_update(self.running_std, std, self.momentum)], x)
+
+                # need broadcasting
+                broadcast_running_mean = K.reshape(self.running_mean, broadcast_shape)
+                broadcast_running_std = K.reshape(self.running_std, broadcast_shape)
+                broadcast_beta = K.reshape(self.beta, broadcast_shape)
+                broadcast_gamma = K.reshape(self.gamma, broadcast_shape)
+                x_normed_running = K.batch_normalization(
+                    x, broadcast_running_mean, broadcast_running_std,
+                    broadcast_beta, broadcast_gamma,
+                    epsilon=self.epsilon)
+
+                # pick the normalized form of x corresponding to the training phase
+                x_normed = K.in_train_phase(x_normed, x_normed_running)
+
+        elif self.mode == 1:
+            # sample-wise normalization
+            m = K.mean(x, axis=-1, keepdims=True)
+            std = K.sqrt(K.var(x, axis=-1, keepdims=True) + self.epsilon)
+            x_normed = (x - m) / (std + self.epsilon)
+            x_normed = self.gamma * x_normed + self.beta
+        #recover the reverse-complemented channels
+        true_x_normed = K.concatenate(
+            tensors=[x_normed[:self.input_len,:,:],
+                     x_normed[self.input_len:,:,:][:,:,::-1]],
+            axis=2)
+        return true_x_normed
+
+    def get_config(self):
+        config = {'epsilon': self.epsilon,
+                  'mode': self.mode,
+                  'axis': self.axis,
+                  'gamma_regularizer': self.gamma_regularizer.get_config() if self.gamma_regularizer else None,
+                  'beta_regularizer': self.beta_regularizer.get_config() if self.beta_regularizer else None,
+                  'momentum': self.momentum}
+        base_config = super(RevCompConv1DBatchNorm, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
